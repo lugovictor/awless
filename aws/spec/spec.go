76d4093a73a3116c23be4d7fb062a2679aed139f
François-Xavier Aguessy <fxaguessy@users.noreply.github.com>
2017-10-19 10:49:37 +0200

    WIP: DSL

--- aws/spec/spec.go
+++ aws/spec/spec.go
@@ -3,6 +3,7 @@ package awsspec
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 
 	"github.com/wallix/awless/cloud"
 )
@@ -68,6 +69,155 @@ func fakeDryRunId(entity string) string {
 	default:
 		return fmt.Sprintf("dryrunid-%d", suffix)
 	}
+}
+
+type paramRule struct {
+	rule   ruleNode
+	extras []string
+}
+
+func (p *paramRule) hint() string {
+	if len(p.extras) == 0 {
+		return p.rule.hint()
+	}
+
+	return fmt.Sprintf("(%s OR %s)", p.rule.hint(), strings.Join(p.extras, " OR "))
+}
+
+func (p *paramRule) unexpected(keys []string) []string {
+	var unexpected []string
+	for _, key := range keys {
+		if p.rule.unexpected(key) && !contains(p.extras, key) {
+			unexpected = append(unexpected, key)
+		}
+	}
+	return unexpected
+}
+
+func (p *paramRule) verify(keys []string) ([]string, error) {
+	var err error
+	if unexpected := p.unexpected(keys); len(unexpected) > 0 {
+		err = fmt.Errorf("unexpected param key(s) '%s' - expected params: %s", strings.Join(unexpected, "', '"), p.hint())
+	}
+	missings, _ := p.rule.eval(keys)
+	return missings, err
+}
+
+type ruleNode interface {
+	hint() string
+	unexpected(string) bool
+	eval([]string) ([]string, int)
+}
+
+type oneOfNode struct {
+	children []ruleNode
+}
+
+func (o oneOfNode) hint() string {
+	var childrenHint []string
+	for _, c := range o.children {
+		childrenHint = append(childrenHint, c.hint())
+	}
+	return fmt.Sprintf("(%s)", strings.Join(childrenHint, " OR "))
+}
+
+func (o oneOfNode) unexpected(s string) bool {
+	for _, c := range o.children {
+		if !c.unexpected(s) {
+			return false
+		}
+	}
+	return true
+}
+
+func (o oneOfNode) eval(keys []string) ([]string, int) {
+	var maxProcessed int
+	minSize := len(keys)
+	var minMissings []string
+	for _, child := range o.children {
+		missings, nbFound := child.eval(keys)
+		if nbFound > maxProcessed && len(missings) < minSize ||
+			nbFound == maxProcessed && len(missings) < minSize ||
+			nbFound > maxProcessed && len(missings) == minSize {
+			minMissings = missings
+			minSize = len(missings)
+			maxProcessed = nbFound
+		}
+	}
+	return minMissings, maxProcessed
+}
+
+type allOfNode struct {
+	children []ruleNode
+}
+
+func (a allOfNode) hint() string {
+	var childrenHint []string
+	for _, c := range a.children {
+		childrenHint = append(childrenHint, c.hint())
+	}
+	return fmt.Sprintf("(%s)", strings.Join(childrenHint, " AND "))
+}
+
+func (a allOfNode) unexpected(s string) bool {
+	for _, c := range a.children {
+		if !c.unexpected(s) {
+			return false
+		}
+	}
+	return true
+}
+
+func (a allOfNode) eval(keys []string) (missings []string, nbFound int) {
+	for _, child := range a.children {
+		cMissings, cFound := child.eval(keys)
+		if len(cMissings) > 0 {
+			missings = append(missings, cMissings...)
+		}
+		nbFound += cFound
+	}
+	return
+}
+
+type stringNode struct {
+	key string
+}
+
+func (k stringNode) hint() string {
+	return fmt.Sprintf("\"%s\"", k.key)
+}
+
+func (k stringNode) unexpected(s string) bool {
+	return k.key != s
+}
+
+func (k stringNode) eval(keys []string) (missings []string, nbFound int) {
+	if contains(keys, k.key) {
+		nbFound++
+		return
+	}
+	missings = append(missings, k.key)
+	return
+}
+
+func oneOf(ifaces ...*paramRule) *paramRule {
+	var children []ruleNode
+	for _, i := range ifaces {
+		children = append(children, i.rule)
+	}
+	return &paramRule{rule: oneOfNode{children: children}}
+}
+
+func allOf(ifaces ...*paramRule) *paramRule {
+	var children []ruleNode
+	for _, i := range ifaces {
+		children = append(children, i.rule)
+	}
+	return &paramRule{rule: allOfNode{children: children}}
+}
+
+func node(key string) *paramRule {
+	return &paramRule{rule: stringNode{key}}
 }
 
 func String(v string) *string {
--- aws/spec/spec_test.go
+++ aws/spec/spec_test.go
@@ -1,6 +1,9 @@
 package awsspec
 
 import (
+	"errors"
+	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -19,6 +22,41 @@ func checkErrs(t *testing.T, errs []error, length int, expected ...string) {
 		}
 		if !found {
 			t.Fatalf("'%s' should be in errors %v", exp, errs)
+		}
+	}
+}
+
+func TestValidationDSL(t *testing.T) {
+	tcases := []struct {
+		rules      *paramRule
+		keys       []string
+		expErr     error
+		expMissing []string
+	}{
+		{allOf(node("one"), node("two")), []string{"one", "two"}, nil, nil},
+		{allOf(node("one"), node("two")), []string{"one", "other"}, errors.New("unexpected param key(s) 'other'"), nil},
+		{allOf(node("one"), node("two")), []string{"one"}, nil, []string{"two"}},
+		{allOf(oneOf(node("role"), node("user"), node("group")), oneOf(node("arn"), allOf(node("access"), node("service")))), []string{"role", "arn"}, nil, nil},
+		{allOf(oneOf(node("role"), node("user"), node("group")), oneOf(node("arn"), allOf(node("access"), node("service")))), []string{"role", "service"}, nil, []string{"access"}},
+		{allOf(oneOf(node("role"), node("user"), node("group")), oneOf(node("arn"), allOf(node("access"), node("service")))), []string{}, nil, []string{"role", "arn"}},
+	}
+	for i, tcase := range tcases {
+		fmt.Println(i, ":", tcase.rules.hint())
+		missing, err := tcase.rules.verify(tcase.keys)
+		if tcase.expErr == nil {
+			if err != nil {
+				t.Fatalf("%d: %s", i+1, err)
+			}
+			if got, want := missing, tcase.expMissing; !reflect.DeepEqual(got, want) {
+				t.Fatalf("%d: got %#v, want %#v", i+1, got, want)
+			}
+		} else {
+			if err == nil {
+				t.Fatalf("%d: expected err, got nil", i+1)
+			}
+			if got, want := err.Error(), tcase.expErr.Error(); !strings.Contains(got, want) {
+				t.Fatalf("%d: got %s, want %s", i+1, got, want)
+			}
 		}
 	}
 }
