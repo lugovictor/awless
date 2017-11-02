82e45c0cdc54980daf66a403e233f0df7b9ddbca
simcap <simcap@fastmail.com>
2017-11-02 16:39:51 +0100

    Migration: subnet

--- aws/spec/subnet_test.go
+++ aws/spec/subnet_test.go
@@ -16,49 +16,7 @@ package awsspec
 
 import (
 	"testing"
-
-	"github.com/aws/aws-sdk-go/service/ec2"
 )
-
-func init() {
-	genTestsParams["createsubnet"] = map[string]interface{}{
-		"cidr":             "10.10.10.10/24",
-		"vpc":              "my-vpc-id",
-		"availabilityzone": "eu-west-1a",
-		"name":             "mysubnet",
-	}
-	genTestsExpected["createsubnet"] = &ec2.CreateSubnetInput{
-		AvailabilityZone: String("eu-west-1a"),
-		CidrBlock:        String("10.10.10.10/24"),
-		VpcId:            String("my-vpc-id"),
-	}
-	genTestsOutputExtractFunc["createsubnet"] = func() interface{} {
-		return &ec2.CreateSubnetOutput{Subnet: &ec2.Subnet{SubnetId: String("res.createsubnet")}}
-	}
-	genTestsOutput["createsubnet"] = "res.createsubnet"
-	genTestsExpected["createsubnet.createtag"] = &ec2.CreateTagsInput{
-		Resources: []*string{String("res.createsubnet")},
-		Tags: []*ec2.Tag{
-			{Key: String("Name"), Value: String("mysubnet")},
-		},
-	}
-
-	genTestsParams["updatesubnet"] = map[string]interface{}{
-		"id":     "my-subnet-id",
-		"public": "true",
-	}
-	genTestsExpected["updatesubnet"] = &ec2.ModifySubnetAttributeInput{
-		MapPublicIpOnLaunch: &ec2.AttributeBooleanValue{Value: Bool(true)},
-		SubnetId:            String("my-subnet-id"),
-	}
-
-	genTestsParams["deletesubnet"] = map[string]interface{}{
-		"id": "my-subnet-id",
-	}
-	genTestsExpected["deletesubnet"] = &ec2.DeleteSubnetInput{
-		SubnetId: String("my-subnet-id"),
-	}
-}
 
 func TestCreateSubnet(t *testing.T) {
 	create := &CreateSubnet{}
